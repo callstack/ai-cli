c3f6f0a32401e61538cabbf5c450945abf9f5e0f
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-02-15 22:43:49 +0100

    feat: add basic commands

--- src/commands/prompt.ts
+++ src/commands/prompt.ts
@@ -77,13 +77,13 @@ async function runInternal(initialPrompt: string, options: PromptOptions) {
     throw new Error(`Provider config not found: ${providerName}.`);
   }
 
-  const actualConfig = {
+  const config = {
     model: options.model ?? initialConfig.model,
     apiKey: initialConfig.apiKey,
     systemPrompt: initialConfig.systemPrompt,
   };
 
-  output.outputVerbose(`Using model: ${actualConfig.model}`);
+  output.outputVerbose(`Using model: ${config.model}`);
 
   const messages: Message[] = [];
 
@@ -92,27 +92,36 @@ async function runInternal(initialPrompt: string, options: PromptOptions) {
     output.outputAiProgress('Thinking...');
 
     messages.push({ role: 'user', content: initialPrompt });
-    const [content, response] = await provider.getChatCompletion(actualConfig, messages);
+    const [content, response] = await provider.getChatCompletion(config, messages);
 
     output.clearLine();
     output.outputVerbose(`Response: ${JSON.stringify(response, null, 2)}`);
     output.outputAi(content ?? '(null)');
     messages.push({ role: 'assistant', content: content ?? '' });
   } else {
-    output.outputAi('Hello, how can I help you? Press Ctrl+C to exit.');
+    output.outputAi('Hello, how can I help you? ');
   }
 
-  if (!options.interactive && initialPrompt) {
+  if (options.interactive || !initialPrompt) {
+    output.outputInfo(
+      'Type "/exit" or press Ctrl+C to exit. Type "/help" to see available commands.'
+    );
+  } else {
     process.exit(0);
   }
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
     const userPrompt = await inputLine('me: ');
+    const isCommand = processCommandIfNeeded(userPrompt, { messages, providerName, config });
+    if (isCommand) {
+      continue;
+    }
+
     output.outputAiProgress('Thinking...');
 
     messages.push({ role: 'user', content: userPrompt });
-    const [content, response] = await provider.getChatCompletion(actualConfig, messages);
+    const [content, response] = await provider.getChatCompletion(config, messages);
     output.clearLine();
     output.outputVerbose(`Response Object: ${JSON.stringify(response, null, 2)}`);
     output.outputAi(content ?? '(null)');
@@ -126,4 +135,58 @@ function filterOutApiKey(key: string, value: unknown) {
   }
 
   return value;
+}
+
+interface CommandContext {
+  messages: Message[];
+  providerName: string;
+  config: {
+    model: string;
+    systemPrompt: string;
+  };
+}
+
+function processCommandIfNeeded(input: string, context: CommandContext): boolean {
+  if (!input.startsWith('/')) {
+    return false;
+  }
+
+  const [command, ...args] = input.split(' ');
+  if (command === '/exit') {
+    process.exit(0);
+    // No need to return.
+  }
+
+  if (command === '/help') {
+    output.outputInfo('Available commands:');
+    output.outputInfo('- /exit: Exit the program');
+    output.outputInfo(
+      '- /reset-context: Reset conversation context: AI replies will forget previous messages'
+    );
+    output.outputInfo('- /verbose [on|off]: Enable or disable verbose output.');
+    return true;
+  }
+
+  if (command === '/reset-context') {
+    // Drop all messages from context.
+    context.messages.length = 0;
+    output.outputInfo('Context reset: AI replies will forget previous messages.', context.messages);
+    return true;
+  }
+
+  if (command === '/verbose') {
+    output.setVerbose(args[0] !== 'off');
+    output.outputInfo(`Verbose mode: ${output.isVerbose() ? 'on' : 'off'}`);
+    return true;
+  }
+
+  if (command === '/info') {
+    output.outputInfo(`Provider: ${context.providerName}, model: ${context.config.model}`);
+    output.outputInfo('System prompt:', context.config.systemPrompt);
+    output.outputVerbose('Current context:', JSON.stringify(context.messages, null, 2));
+    return true;
+  }
+
+  output.outputError(`Unknown command: ${command} ${args.join(' ')}`);
+  return true;
 }
