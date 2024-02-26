20babdd12c21edf5275349915ec69a5e546d310d
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-02-26 12:15:39 +0100

    chore: add links

--- src/default-config.ts
+++ src/default-config.ts
@@ -1,4 +1,7 @@
+// OpenAI models: https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
 export const DEFAULT_OPEN_AI_MODEL = 'gpt-4';
+
+// Perplexity models: https://docs.perplexity.ai/docs/model-cards
 export const DEFAULT_PERPLEXITY_MODEL = 'sonar-medium-chat';
 
 export const DEFAULT_SYSTEM_PROMPT =
