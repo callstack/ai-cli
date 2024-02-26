7d58d5f67a09787052f3bc5e5257c81e2204ca05
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-02-26 12:10:43 +0100

    feat: update default pplx model after updated offering

--- src/default-config.ts
+++ src/default-config.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_OPEN_AI_MODEL = 'gpt-4';
-export const DEFAULT_PERPLEXITY_MODEL = 'pplx-70b-online';
+export const DEFAULT_PERPLEXITY_MODEL = 'sonar-medium-chat';
 
 export const DEFAULT_SYSTEM_PROMPT =
   'You are a helpful assistant responding in a concise manner to user questions.';
