7e8792b9f8000998295f550d9ab1b22159930798
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2024-02-26 12:16:45 +0100

    feat: update default pplx model after updated offering (#21)
    * feat: update default pplx model after updated offering
    
    * chore: add links

--- src/default-config.ts
+++ src/default-config.ts
@@ -1,5 +1,8 @@
+// OpenAI models: https://platform.openai.com/docs/models/gpt-4-and-gpt-4-turbo
 export const DEFAULT_OPEN_AI_MODEL = 'gpt-4';
-export const DEFAULT_PERPLEXITY_MODEL = 'pplx-70b-online';
+
+// Perplexity models: https://docs.perplexity.ai/docs/model-cards
+export const DEFAULT_PERPLEXITY_MODEL = 'sonar-medium-chat';
 
 export const DEFAULT_SYSTEM_PROMPT =
   'You are a helpful assistant responding in a concise manner to user questions.';
