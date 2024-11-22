610a778fdd7d746bee88236858d4505fd4c93ce1
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2024-11-22 23:17:09 +0000

    chore: update models 2024-11

--- src/engine/providers/anthropic.ts
+++ src/engine/providers/anthropic.ts
@@ -8,17 +8,23 @@ const Anthropic: Provider = {
   name: 'anthropic',
   apiKeyUrl: 'https://console.anthropic.com/settings/keys',
 
-  // Anthropic models: https://docs.anthropic.com/en/docs/about-claude/models
-  defaultModel: 'claude-3-5-sonnet-20240620',
+  // Anthropic models: https://docs.anthropic.com/claude/docs/models-overview
+  defaultModel: 'claude-3-5-sonnet-latest',
 
   // Price per 1M tokens [input, output].
-  // Source: https://docs.anthropic.com/en/docs/about-claude/models
+  // Source: https://www.anthropic.com/api
   modelPricing: {
-    // Current models
-    'claude-3-5-sonnet-20240620': { inputTokensCost: 3.0, outputTokensCost: 15.0 },
-    'claude-3-haiku-20240307': { inputTokensCost: 0.25, outputTokensCost: 1.25 },
-    'claude-3-sonnet-20240229': { inputTokensCost: 3.0, outputTokensCost: 15.0 },
+    // Claude 3.5
+    'claude-3-5-sonnet-latest': { inputTokensCost: 3, outputTokensCost: 15 },
+    'claude-3-5-sonnet-20241022': { inputTokensCost: 3, outputTokensCost: 15 },
+    'claude-3-5-haiku-latest': { inputTokensCost: 1, outputTokensCost: 5 },
+    'claude-3-5-haiku-20241022': { inputTokensCost: 1, outputTokensCost: 5 },
+
+    // Claud 3
+    'claude-3-opus-latest': { inputTokensCost: 15.0, outputTokensCost: 75.0 },
     'claude-3-opus-20240229': { inputTokensCost: 15.0, outputTokensCost: 75.0 },
+    'claude-3-sonnet-20240229': { inputTokensCost: 3.0, outputTokensCost: 15.0 },
+    'claude-3-haiku-20240307': { inputTokensCost: 0.25, outputTokensCost: 1.25 },
 
     // Legacy models
     'claude-2.1': { inputTokensCost: 8.0, outputTokensCost: 24.0 },
@@ -27,8 +33,8 @@ const Anthropic: Provider = {
   },
 
   modelAliases: {
-    haiku: 'claude-3-haiku-20240307',
-    sonnet: 'claude-3-5-sonnet-20240620',
+    haiku: 'claude-3-5-haiku-latest',
+    sonnet: 'claude-3-5-sonnet-latest',
     opus: 'claude-3-opus-20240229',
   },
 
